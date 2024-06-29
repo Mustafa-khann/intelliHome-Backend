8542b635fc4f548469e6728352d577bf3b31e1ec
Mustafa-khann <mustafakhann050@gmail.com>
2024-06-29 12:41:39 +0500

    minor changes

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.get('/', (req, res) => {
   res.send('Smart Home MVP Backend');
 });
 
-// Start server
+// Start server on port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
