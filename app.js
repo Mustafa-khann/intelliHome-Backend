f5812ce001281b88480957ba61f0d47131e95149
Mustafa-khann <mustafakhann050@gmail.com>
2024-06-29 12:21:25 +0500

    basic server

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const mqtt = require('mqtt');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/smart_home_mvp', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Connect to MQTT broker
+const client = mqtt.connect('mqtt://localhost');
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Smart Home MVP Backend');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
