affe1b76f542d8f6cc5b368806fbee500f623363
Mustafa-khann <mustafakhann050@gmail.com>
2024-06-30 09:09:35 +0500

    dev build ready

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.get('/', (req, res) => {
   res.send('Smart Home mvpp Backend');
 });
 
-// Start server
+// Start server on port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
